6f65e06edbaf643ff72c63920904645c3d4edd05
AngeloGrabner <angelo.grabner@gmx.de>
2022-02-20 18:28:31 +0100

    this is taking longer than i expected

--- tictactoe-opp/Display.cs
+++ tictactoe-opp/Display.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 /*
      u2588 is the block 
 
@@ -56,52 +57,135 @@
 internal static class Display
 {
     private static char[] f; //fild
-    private static int width = 89, height = 29;
+    private static int[] _gameState = new int[0];
+    private const int _width = 59, _height = 29;
     
     static Display()
     {
-        if (Console.LargestWindowHeight < height)
+        if (Console.LargestWindowHeight < _height+1)
         {
-            height = Console.LargestWindowHeight;
-        }
-        if (Console.LargestWindowWidth < width)
-        {
-            width = Console.LargestWindowWidth;
-        }
-        Console.CursorVisible = false;
-        Console.OutputEncoding = System.Text.Encoding.Unicode;
-        f = new char[width * height];
-    }
-    public static void update(int[] GameState, bool playerTurn)
-    {
-        Console.Clear();
-        if (!playerTurn) // should be right
-        {
-            Console.Write("It's the turn of player ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("One!\n");
-            Console.ForegroundColor = ConsoleColor.White;
+            #pragma warning disable CA1416 // only works on windows
+            Console.WindowHeight = Console.LargestWindowHeight;
+
         }
         else
         {
-            Console.Write("It's the turn of player ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Two!\n");
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.WindowHeight = _height+2;
         }
-        draw();
-        Console.WriteLine(f);
+        if (Console.LargestWindowWidth < _width)
+        {
+            Console.WindowWidth = Console.LargestWindowWidth;
+        }
+        else
+        {
+            Console.WindowWidth = _width;
+            #pragma warning restore CA1416 
+        }
+        Console.CursorVisible = false;
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        f = new char[_width * _height];
     }
-    private static int Convert(int x, int y) // makes a 2d coordinate into a 1d one 
+    public static void update(int[] gameState, bool playerTurn)
     {
-        return y * 58 + x; // 58 is the max lenght of x
+        _gameState = gameState;
+        ConsoleColor c;
+        if (playerTurn)
+        {
+
+        }
+        else
+        {
+
+        }
+
+        Console.Clear();
+        draw();
+        Console.Write(f);
+    }
+    private static int convert(int x, int y) // makes a 2d coordinate into a 1d one 
+    {
+        return y * 59 + x; // 59 is the max lenght of x
     }
     private static void draw()
     {
-        for (int i = 0; i < f.Length; i++)
+        //for (int i = 0; i < 58; i++) //for mesurements 
+        //    Console.Write(i % 10);
+        //Console.WriteLine();
+        for (int i = 0; i < f.Length; i++) // clearing buffer
             f[i] = ' ';
-        for (int i = 58; i < f.Length; i += Convert(0, 1))
+
+        for (int i = _width-1; i < f.Length; i += _width)
             f[i] = '\n';
+        verticalLine(18); // left line 
+        verticalLine(19);
+        verticalLine(38); // right line
+        verticalLine(39);
+
+        horizontalLine(9); // top line
+        horizontalLine(19); // bottom line 
+        for (int i = 0; i < _gameState.Length; i++)
+        {
+            if (_gameState[i] == 0)
+            {
+                continue;
+            }
+            else if (_gameState[i] == 1)
+            {
+                draw_X(i%3,i/3);
+            }
+            else if (_gameState[i] == 2)
+            {
+                draw_O(i%3,i/3);
+            }
+        }
 
     }
+
+    private static void verticalLine(int x)
+    {
+        for (int i = x; i < f.Length;i+=_width)
+        {
+            f[i] = '\u2588';
+        }
+    }
+    private static void horizontalLine(int y)
+    {
+        for (int i = convert(0,y); i < convert(_width-1,y) ; i++)
+        {
+            f[i] = '\u2588';
+        }
+    }
+    private static void draw_X(int x, int y) // yes it is hard coded and hopfully works
+    {
+        char c = '\u2588';
+        for (int i = 6; i < 12; i++)
+        {
+            f[convert(x * 20 + i, y * 10 + 1)] = c; // upper line 
+            f[convert(x * 20 + i, y * 10 + 7)] = c; //lower line
+        }
+        f[convert(x * 20 + 4, y * 10 + 2)] = c; // upper left 
+        f[convert(x * 20 + 5, y * 10 + 2)] = c;
+        
+        f[convert(x * 20 + 12, y * 10 + 2)] = c; //upper right 
+        f[convert(x * 20 + 13, y * 10 + 2)] = c;
+        for (int i = 3; i < 6; i++) // side lines 
+        {
+            f[convert(x * 20 + 2, y * 10 + i)] = c;
+            f[convert(x * 20 + 3, y * 10 + i)] = c;
+
+            f[convert(x * 20 + 14, y * 10 + i)] = c;
+            f[convert(x * 20 + 15, y * 10 + i)] = c;
+        }
+
+        f[convert(x * 20 + 4, y * 10 + 6)] = c; // lower left 
+        f[convert(x * 20 + 5, y * 10 + 6)] = c;
+
+        f[convert(x * 20 + 12, y * 10 + 6)] = c; // lower right 
+        f[convert(x * 20 + 13, y * 10 + 6)] = c;
+    }
+    private static void draw_O(int x, int y) // this one as well
+    {
+
+    }
+
 }
--- tictactoe-opp/Program.cs
+++ tictactoe-opp/Program.cs
@@ -6,8 +6,11 @@ namespace TicTacToe
     {
         static void Main()
         {
-            Game game = new Game();
-            game.run();
+            //Game game = new Game();
+            //game.run();
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, };
+            Display.update(a, true);
+            Console.ReadLine();
         }
     }
 }
