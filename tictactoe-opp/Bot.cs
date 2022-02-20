c44e1e504e2567e12b3967b769aee80f8597fa9c
AngeloGrabner <angelo.grabner@gmx.de>
2022-02-20 12:29:33 +0100

    split into more files

---
+++ tictactoe-opp/Bot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+internal class Bot
+{
+    public Bot()
+    {
+
+    }
+    public void setState(int[] state)
+    {
+
+    }
+    public int getTurn()
+    {
+        return 0;
+    }
+}
